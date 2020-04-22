32414349790d10d22a2795fbc250c63df4ece08e
amrit.talwar <amrit.talwar@wonderbill.com>
2020-04-22 13:07:55 +0100

    change db init logic to allow for multiple executions without error
    use IF NOT EXISTS so we dont get errors when running dbinit.sh after the first execution

--- db/dbinit.sh
+++ db/dbinit.sh
@@ -3,16 +3,18 @@
 docker-compose up -d
 
 # wait for postgres setup to complete
+echo "Sleeping for 5 seconds to allow db setup"
 sleep 5
 
 # exec into running postgresql container and create Users table
 # TODO: Use an ORM to automate db building because this script is a fucking pain
 docker-compose exec postgresql bash -c "
   psql -d apidatabase -U apiusername -c '
-  CREATE TABLE Users (
+  CREATE TABLE IF NOT EXISTS Users (
     user_id serial PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(256) NOT NULL
   );'
 "
+echo "Setup complete, query away!"
